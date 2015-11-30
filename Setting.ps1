2cc3d07228d7f26a2c12ae1f46fe44f0125bc081
wherby <187225577@qq.com>
2015-11-30 21:15:09 +0800

    move goto to top dir

---
+++ Setting.ps1
@@ -0,0 +1,10 @@
+$paths=@{"python"='C:\Python27';"home"='C:\Users\zhoust';"columbus"="C:\Users\zhoust\Documents\GitHub\Columbus";
+           "internal"="C:\cygwin64\home\zhoust;C:\cygwin64\bin\mintty.exe ssh -i ccolum.pem ec2-user@ec2-54-152-69-51.compute-1.amazonaws.com";
+           "yoeman"='C:\Users\zhoust\Documents\yeoman';
+           "pshome"='C:\Users\zhoust\Documents\GitHub\Script-master\Powershell\GoTo';
+           "sdk"='C:\Users\zhoust\Documents\GitHub\Platform-SDK-JS';
+           "pportal"='C:\Users\zhoust\Documents\GitHub\Platform-SDK-Portal';
+           "settion"='C:\Users\zhoust\Documents\GitHub\Script-master\Powershell\GoTo\';
+           "ruby"='D:\Coursera\Ruby\RubyOnRails';
+           "scalaTest"='C:\Users\zhoust\Documents\Temp\TEMPTest;notepad .\Main.scala';
+           "debug"='C:\Users\zhoust\Documents\Temp\TEMPTest;scalac -Xprint:parse Main.scala'}
